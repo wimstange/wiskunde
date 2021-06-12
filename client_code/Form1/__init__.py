8e030ec673a57aeb6148e791ed73d0b212884835
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-12 15:01:08 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -15,17 +15,17 @@ class Form1(Form1Template):
     """This method is called when the button is clicked"""
     ### Lissajous figuur zonder gebruik van numpy, met gebruik matplotlib.pyplot
 
-    def lissajous(a,b,c,d,e):
+    
+    self.plot_1.data = self.lissajous(5,14,32,7,1000)
+    
+  def lissajous(self,a,b,c,d,e):
       x, y = [], []
       for i in range(e):
         t = 2*(i)/e*math.pi
         x.append(a*math.sin(c*t))
         y.append(b*math.cos(d*t))
-      fig = go.Figure(data=go.Scatter(x=x, y=y))
+      fig = go.Scatter(x=x, y=y)
       return(fig)
-
-    self.plot_1.data = lissajous(4,14,32,7,1000)
-    
 
 
 
