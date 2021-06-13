00d93f36d2aca8138f0449e2d8ad69c6814797e5
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-13 07:36:23 +0000

    Created Form1; Edited settings

---
+++ client_code/Form1/__init__.py
@@ -0,0 +1,15 @@
+from ._anvil_designer import Form1Template
+from anvil import *
+
+class Form1(Form1Template):
+  def __init__(self, **properties):
+    # Set Form properties and Data Bindings.
+    self.init_components(**properties)
+   
+  def
+  
+  
+  c=self.canvas_1
+    
+    # Any code you write here will run when the form opec
+    c.fill_text('WimStange',100,100)
