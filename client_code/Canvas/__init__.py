ab9cc066ced45e565f2ee4e3c017221ebc21711b
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-13 09:51:59 +0000

    Edited Canvas, settings and Krommen

--- client_code/Canvas/__init__.py
+++ client_code/Canvas/__init__.py
@@ -5,23 +5,24 @@ class Canvas(CanvasTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
-# Any code you write here will run when the form opec
+
+    # Any code you write here will run when the form opens.
+    
 
   def canvas_1_show(self, **event_args):
     """This method is called when the Canvas is shown on the screen"""
     c = self.canvas_1
+  
     c.begin_path()
     c.move_to(100,100)
     c.line_to(100,200)
     c.line_to(200,200)
     c.close_path()
+  
     c.stroke_style = "#2196F3"
     c.line_width = 3
     c.fill_style = "#E0E0E0"
+  
     c.fill()
     c.stroke()
-    c.line_width=4
-    c.font='72px'
-    c.fill_text('WimStange',200,150)
-  
 
--- client_code/Krommen/__init__.py
+++ client_code/Krommen/__init__.py
@@ -2,6 +2,7 @@ from ._anvil_designer import KrommenTemplate
 from anvil import *
 from ..Hypocycloide import Hypocycloide
 from ..Lissajous import Lissajous
+from ..Canvas import Canvas
 
 class Krommen(KrommenTemplate):
   def __init__(self, **properties):
@@ -20,5 +21,19 @@ class Krommen(KrommenTemplate):
     """This method is called when the link is clicked"""
     self.content_panel.clear()
     self.content_panel.add_component(Lissajous())
+
+  def form_show(self, **event_args):
+    """This method is called when the HTML panel is shown on the screen"""
+    pass
+
+  def link_3_click(self, **event_args):
+    """This method is called when the link is clicked"""
+    self.content_panel.clear()
+    self.content_panel.add_component(Canvas())
+
+
+
+
+
 
 
