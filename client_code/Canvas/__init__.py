90930ccb4f3f65b4102c7fb97ad209bb6ce6d6cc
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-13 20:19:16 +0000

    Edited Canvas

--- client_code/Canvas/__init__.py
+++ client_code/Canvas/__init__.py
@@ -8,35 +8,40 @@ class Canvas(CanvasTemplate):
     self.init_components(**properties)
 
     # Any code you write here will run when the form opens.
-    
-
+   
+  def transform(self, points,multp,add):
+    result = []
+    for point in points:
+        result.append((multp*point[0]+add,multp*point[1]+add+60))
+    return result
+  
   def canvas_1_show(self, **event_args):
     """This method is called when the Canvas is shown on the screen"""
     c = self.canvas_1
+    MULTP = 64
+    ADD = 20
+    DISTANCE = 20 
+    pointsBase = [(2,0),(4,0),(6,0),
+              (1,2),(3,2),(5,2),(7,2),
+              (0,4),(2,4),(4,4),(6,4),(8,4),
+              (1,6),(3,6),(5,6),(7,6),
+              (2,8),(4,8),(6,8)]
+    
+    points = self.transform(pointsBase,MULTP,ADD)
+    
+    
+    
+    for p in pointsBase:
+      c.begin_path()
+      c.arc(p[0],p[1], 0, 2*math.pi,True)
+    
   
-    c.begin_path()
-    c.move_to(100,100)
-    c.line_to(100,200)
-    c.line_to(200,200)
+      c.stroke_style = "#2196F3"
+      c.line_width = 3
+      c.fill_style = "rgba(255,0,0,1)"
+  
+      c.fill()
+      c.stroke()
+    
     c.close_path()
-    c.stroke_style = "#2196F3"
-    c.line_width = 3
-    c.fill_style = "#E0E0E0"
-  
-    c.fill()
-    c.stroke()
-    
-    
-    c.begin_path()
-    c.arc(500, 200, 10, 0, 2*math.pi,True)
-    c.close_path()
-  
-    c.stroke_style = "#2196F3"
-    c.line_width = 3
-    c.fill_style = "rgba(255,0,0,1)"
-  
-    c.fill()
-    c.stroke()
-    
-    
 
