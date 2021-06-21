00d31a45ce3d264afb268847ee7bb47ef0acaa71
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-21 17:02:46 +0000

    Created Nieuwe_wereld; Edited Leven; Deleted Leven.Nieuwe_wereld

--- client_code/Leven/__init__.py
+++ client_code/Leven/__init__.py
@@ -1,7 +1,7 @@
 from ._anvil_designer import LevenTemplate
 from anvil import *
 import random
-from ..Ni
+from ..Nieuwe_wereld import Nieuwe_wereld
 class Leven(LevenTemplate):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
