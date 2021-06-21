003a64c735094b6b53481f5979cf3e33be5f4d82
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-21 10:50:50 +0000

    Edited Leven

--- client_code/Leven/__init__.py
+++ client_code/Leven/__init__.py
@@ -7,14 +7,8 @@ class Leven(LevenTemplate):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
     self.N = 80
+    self.wereld = []
     # Any code you write here will run when the form opens.
-    
-
-  def canvas_1_show(self, **event_args):
-    """This method is called when the Canvas is shown on the screen"""
-    c = self.canvas_1
-#    N = 80
-    wereld = []
     for i in range(self.N):
         r = []
         for j in range(self.N):
@@ -23,14 +17,18 @@ class Leven(LevenTemplate):
               r.append(0)
             else:
               r.append(255)
-        wereld.append(r)
+        self.wereld.append(r)
+
+  def canvas_1_show(self, **event_args):
+    """This method is called when the Canvas is shown on the screen"""
+    c = self.canvas_1
          
     for i in range(self.N):
       for j in range(self.N):
-        if wereld[i][j] == 255:
+        if self.wereld[i][j] == 0:
           kleur = "rgba(255,0,0,1)"
         else: 
-          kleur = "rgba(0,0,25,1)"
+          kleur = "rgba(50,200,25,1)"
         c.begin_path()
         c.move_to(i*10,j*10)
         c.line_to(i*10+10,j*10)
@@ -47,6 +45,22 @@ class Leven(LevenTemplate):
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    pass
+    
+    nieuweWereld = self.wereld.copy()
+    for i in range(self.N):
+        for j in range(self.N):
+            waarde = int((self.wereld[i][(j-1)%self.N]+self.wereld[i][(j+1)%self.N]+self.wereld[(i-1)%self.N][j]+
+                          self.wereld[(i+1)%self.N][j]+self.wereld[(i-1)%self.N][(j-1)%self.N]+self.wereld[(i-1)%self.N][(j+1)%self.N]+
+                          self.wereld[(i+1)%self.N][(j-1)%self.N]+self.wereld[(i+1)%self.N][(j+1)%self.N])/255)
+            if self.wereld[i][j] == 255:
+                if waarde <2 or waarde > 3:
+                    nieuweWereld[i][j] = 0
+            else:
+                if waarde == 3:
+                    nieuweWereld[i][j] = 255
+                    
+    self.wereld = nieuweWereld
+    self.canvas_1_show()
+    
 
 
