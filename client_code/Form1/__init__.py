a73e88f950392cba36555ee7eda37a34a854df7d
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-23 08:38:32 +0000

    Edited Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -1,6 +1,7 @@
 from ._anvil_designer import Form1Template
 from anvil import *
 from ..Form1_detail import Form1_detail
+from Pillow import Image
 
 class Form1(Form1Template):
   def __init__(self, **properties):
