cb3eb4d7d84890da00564729bb42166c80996019
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-23 04:49:07 +0000

    Edited settings and Form1

--- client_code/Form1/__init__.py
+++ client_code/Form1/__init__.py
@@ -11,5 +11,18 @@ class Form1(Form1Template):
 
   def button_1_click(self, **event_args):
     """This method is called when the button is clicked"""
-    alert(content="Form1_detail")
+    item_copy = dict(list(self.item))
+    save_clicked = alert(
+      content=Form1_detail(item=item_copy),
+      title="Het detail formulier",
+      large=True,
+      buttons=[("Save",True),("Cancel",False)],
+    )
+    if save_clicked:
+    
 
+      self.refresh_data_bindings()
+
+    
+    
+ 
