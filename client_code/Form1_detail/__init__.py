0a96f4b27d9a6958cd312499f5dc6135150a1aa5
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-23 04:52:32 +0000

    Edited Form1_detail

--- client_code/Form1_detail/__init__.py
+++ client_code/Form1_detail/__init__.py
@@ -1,7 +1,7 @@
 from ._anvil_designer import Form1_detailTemplate
 from anvil import *
 
-class Form1_detail(Form1_detailTemplate):
+class Form1_detail(Form1_detailTemplate, item=new_items):
   def __init__(self, **properties):
     # Set Form properties and Data Bindings.
     self.init_components(**properties)
