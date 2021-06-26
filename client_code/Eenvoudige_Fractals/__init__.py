0ff436125219dbe727bbc09d6507ab0460f293c3
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-26 17:40:32 +0000

    Edited Eenvoudige_Fractals

--- client_code/Eenvoudige_Fractals/__init__.py
+++ client_code/Eenvoudige_Fractals/__init__.py
@@ -11,11 +11,11 @@ class Eenvoudige_Fractals(Eenvoudige_FractalsTemplate):
     # Any code you write here will run when the form opens.
     axioma = self.axioma.text
     schema = self.schema.text
-
     hoek_gr = int(self.hoek_gr.text)
     hoek = float(hoek_gr)/180 * m.pi
     iteraties = int(self.iteraties.text)
-    self.voorschrift = self.productie(axioma,schema,4)
+    lengte = float(self.lengte.text)
+    self.voorschrift = self.productie(axioma,schema,iteraties)
     
   def volgende(self, woord, schema):
     for c in woord:
@@ -40,16 +40,16 @@ class Eenvoudige_Fractals(Eenvoudige_FractalsTemplate):
     
     c = self.canvas_1
     richting = 0
-    lengte =3
-    hoek_gr = int(self.hoek_gr.text)
-    hoek = float(hoek_gr)/180 * m.pi
-    x = 0
-    y = 0
+#   lengte =3
+#    hoek_gr = int(self.hoek_gr.text)
+#    hoek = float(hoek_gr)/180 * m.pi
+    x = 300
+    y = 300
     kleur = rood
 
         
     for k in self.voorschrift:
-        x_n, y_n = x+lengte*m.cos(richting),y+lengte*m.sin(richting)
+        x_n, y_n = x+float(self.lengte.text)*m.cos(richting),y+float((self.lengte.text))*m.sin(richting)
         if k == "F":
             c.begin_path()
             c.move_to(x,y)  
@@ -67,22 +67,25 @@ class Eenvoudige_Fractals(Eenvoudige_FractalsTemplate):
         elif k == "f":
             x, y = x_n, y_n
         elif k == "+":
-            richting += hoek
+            richting += float(self.hoek_gr.text)/180 * m.pi
         elif k == "-":
-            richting -= hoek
+            richting -= float(self.hoek_gr.text)/180 * m.pi
 
   def axioma_pressed_enter(self, **event_args):
     """This method is called when the user presses Enter in this text box"""
-    axioma = self.axioma.text
-    schema = self.schema.text
-    self.voorschrift = self.productie(axioma,schema,2)
-    print(self.voorschrift)
+#    axioma = self.axioma.text
+#    schema = self.schema.text
+#    self.voorschrift = self.productie(axioma,schema,2)
+#    print(self.voorschrift)
 #    self.canvas_1.reset_context
-    self.canvas_1_show
+#    self.canvas_1_show
 
   def button_1_click(self, **event_args):
-    """This method is called when the button is clicked"""
-    
+    """Deze methode wordt uitgevoerd als op de button 'teken fractal' wordt geklikt"""
+    if 5 < int(self.iteraties.text) or int(self.iteraties.text) <1:
+      alert("Kies een iteratiewaarde tussen 1 en 6")
+                   
+    self.voorschrift = self.productie(self.axioma.text,self.schema.text,int(self.iteraties.text))
     self.canvas_1.reset_context()
     self.canvas_1_show()
 
