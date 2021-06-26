7cbc7b5853b5921e929f60cd10954b4f716b43e8
wimstange53@gmail.com <wimstange53@gmail.com>
2021-06-26 14:26:47 +0000

    Edited Eenvoudige_Fractals

--- client_code/Eenvoudige_Fractals/__init__.py
+++ client_code/Eenvoudige_Fractals/__init__.py
@@ -15,7 +15,7 @@ class Eenvoudige_Fractals(Eenvoudige_FractalsTemplate):
     hoek_gr = int(self.hoek_gr.text)
     hoek = float(hoek_gr)/180 * m.pi
     iteraties = int(self.iteraties.text)
-    self.voorschrift = self.productie(axioma,schema,iteraties)
+    self.voorschrift = self.productie(axioma,schema,4)
     
   def volgende(self, woord, schema):
     for c in woord:
@@ -23,72 +23,36 @@ class Eenvoudige_Fractals(Eenvoudige_FractalsTemplate):
             alert("ongeldige string")
     return woord.replace("F", schema)
 
-  def productie(self, woord, schema, iteratie):
+  def productie(self, woord, schema, iteraties):
     resultaat = woord
-    for i in range(iteratie):
+    for i in range(iteraties):
         resultaat = self.volgende(resultaat,schema)
     return resultaat
         
-  def teken(self, x,y,voorschrift, kleur):
-    c = self.canvas_1
-    lengte = 25
-    richting = 0
-    x = x
-    y = y
-    for k in voorschrift:
-        x_n, y_n = x+lengte*m.cos(richting),y+lengte*m.sin(richting)
-        if k == "F":
-            c.begin_path()
-            c.line_to(x_n, y_n)
-            c.close_path()
-  
-            c.stroke_style = kleur
-            c.line_width = 3
-            c.fill_style = kleur
-  
-            c.fill()
-            c.stroke()
-
-            x, y = x_n, y_n
-        elif k == "f":
-            x, y = x_n, y_n
-        elif k == "+":
-            richting += hoek
-        elif k == "-":
-            richting -= hoek
-    
-# Definieer kleuren
-
-  zwart    = (   0,   0,   0)
-  wit      = ( 255, 255, 255)
-  groen    = (   0, 255,   0)
-  rood     = ( 255,   0,   0)
-
-
   def canvas_1_show(self, **event_args):
     """This method is called when the Canvas is shown on the screen"""
     # Definieer kleuren
 
-    zwart    = (   0,   0,   0)
-    wit      = ( 255, 255, 255)
-    groen    = (   0, 255,   0)
-    rood     = ( 255,   0,   0)
+    zwart    = "rgba(   0,   0,   0)"
+    wit      = "rgba( 255, 255, 255)"
+    groen    = "rgba(   0, 255,   0)"
+    rood     = "rgba(( 255,   0,   0)"
     
     c = self.canvas_1
     richting = 0
-    lengte = 25
+    lengte = 10
     hoek_gr = int(self.hoek_gr.text)
     hoek = float(hoek_gr)/180 * m.pi
-    x = 150
-    y = 150
-    kleur = zwart
-    print(self.voorschrift)
+    x = 0
+    y = 0
+    kleur = rood
+
         
     for k in self.voorschrift:
-        print(k, lengte, richting, hoek)
         x_n, y_n = x+lengte*m.cos(richting),y+lengte*m.sin(richting)
         if k == "F":
             c.begin_path()
+            c.move_to(x,y)  
             c.line_to(x_n, y_n)
             c.close_path()
   
@@ -106,4 +70,14 @@ class Eenvoudige_Fractals(Eenvoudige_FractalsTemplate):
             richting += hoek
         elif k == "-":
             richting -= hoek
+
+  def axioma_pressed_enter(self, **event_args):
+    """This method is called when the user presses Enter in this text box"""
+    axioma = self.axioma.text
+    schema = self.schema.text
+    self.voorschrift = self.productie(axioma,schema,2)
+    print(self.voorschrift)
+#    self.canvas_1.reset_context
+    self.canvas_1_show
+
 
