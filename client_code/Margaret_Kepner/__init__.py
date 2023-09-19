dec702ec856c82c9800f27a6f8f608b610216acc
wimstange53@gmail.com <wimstange53@gmail.com>
2023-09-19 20:24:54 +0000

    Edited settings and Margaret_Kepner

--- client_code/Margaret_Kepner/__init__.py
+++ client_code/Margaret_Kepner/__init__.py
@@ -6,27 +6,26 @@ class Margaret_Kepner(Margaret_KepnerTemplate):
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-        self.N = 80
-        self.wereld = []
-
+        self.kleur = "rgba(255,0,0,1)"
+        self.a=300
+        self.b=400
         # Any code you write here will run when the form opens.
     def canvas_1_show(self, **event_args):
         """This method is called when the Canvas is shown on the screen"""
-        c = self.canvas_1
          
-        kleur = "rgba(255,0,0,1)"
-        i=3
-        j=4
+    def maak_vierkant(self):
+        c = self.canvas_1
         c.begin_path()
-        c.move_to(i*10,j*10)
-        c.line_to(i*10+10,j*10)
-        c.line_to(i*10+10,j*10+10)
-        c.line_to(i*10,j*10+10)
+        c.move_to(self.a,self.b)
+        c.line_to(self.a+10,self.b)
+        c.line_to(self.a+10,self.b+10)
+        c.line_to(self.a,self.b+10)
         c.close_path()
   
-        c.stroke_style = kleur
+        c.stroke_style = self.kleur
         c.line_width = 1
-        c.fill_style = kleur
+        c.fill_style = self.kleur
   
         c.fill()
         c.stroke()
+        c.show()
