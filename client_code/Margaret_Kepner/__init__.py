6ab33874a0fdb4f74bf30141592f0db69d7221be
wimstange53@gmail.com <wimstange53@gmail.com>
2023-09-20 15:11:14 +0000

    Edited Margaret_Kepner

--- client_code/Margaret_Kepner/__init__.py
+++ client_code/Margaret_Kepner/__init__.py
@@ -3,30 +3,16 @@ from anvil import *
 import anvil.server
 
 class Margaret_Kepner(Margaret_KepnerTemplate):
+   
     def __init__(self, **properties):
         # Set Form properties and Data Bindings.
         self.init_components(**properties)
-        self.kleur = "rgba(255,0,0,1)"
-        self.a=300
-        self.b=400
-        # Any code you write here will run when the form opens.
-    def canvas_1_show(self, **event_args):
-        """This method is called when the Canvas is shown on the screen"""
-        self.maak_vierkant()
-         
-    def maak_vierkant(self):
-        c = self.canvas_1
-        c.begin_path()
-        c.move_to(self.a,self.b)
-        c.line_to(self.a+10,self.b)
-        c.line_to(self.a+10,self.b+10)
-        c.line_to(self.a,self.b+10)
-        c.close_path()
-  
-        c.stroke_style = self.kleur
-        c.line_width = 1
-        c.fill_style = self.kleur
-  
-        c.fill()
-        c.stroke()
         
+        # Lijst met punten voor je figuur
+        self.points = [(100, 100), (150, 150), (200, 100), (250, 150)]
+    def canvas_1_show(self, **event_args):
+        # Teken de lijnen tussen de punten
+        for i in range(len(self.points) - 1):
+            x1, y1 = self.points[i]
+            x2, y2 = self.points[i + 1]
+            self.canvas_1.draw_line(x1, y1, x2, y2, color="black", width=2)        
